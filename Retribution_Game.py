c9c02e6464a5855902302663bf9583205599b8c3
DrakeC <s581300@student.roundrockisd.org>
2014-12-01 13:38:02 -0600

    DO NOT RUN
    This will crash Canopy.

--- Retribution_Game.py
+++ Retribution_Game.py
@@ -9,13 +9,23 @@
 #show(pict)
             
 #print "you open up the cell doors and walk out.. thinking about what you have just been told"
+
+# Name System
 nameName = False
 while nameName == False:
     print 'What is your name?'
     name = str(raw_input())
+    while name == 'falcon punch' or 'Captain Falcon':
+        print ' '
+        print '*******************'
+        print 'Show me your moves!'
+        print '*******************'
+        print ' '
     print "So your name is " + name + ", yes or no?"
     yesOrNo = str(raw_input())
     if yesOrNo == 'yes' or yesOrNo == 'Yes':
         nameName = True
     else:
         nameName = False
+        
+
